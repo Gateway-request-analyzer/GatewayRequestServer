c1648c5f75406afc03c8def99b13f88cf5a0e706
Vhagenbo <98605236+Hagenbo@users.noreply.github.com>
2022-12-05 09:36:08 -0500

    updated createClient

--- src/main/java/com/Gateway_request_analyzer/starter/rateLimiter.java
+++ src/main/java/com/Gateway_request_analyzer/starter/rateLimiter.java
@@ -2,12 +2,13 @@ package com.Gateway_request_analyzer.starter;
 import io.vertx.core.Vertx;
 import io.vertx.redis.client.Redis;
 import io.vertx.redis.client.RedisAPI;
+import io.vertx.redis.client.RedisOptions;
 
 
 public class rateLimiter {
 
   //Allow for rate limiting on IP, user identifier, and user session
-  /*private static Jedis jedis = new Jedis("http://localhost:6379/");
+  /*
   private static int epoch_ms;
   private static Pipeline pipe;*/
   private Vertx vertx;
@@ -22,7 +23,7 @@ public class rateLimiter {
           // The client handles REDIS URLs. The select database as per spec is the
           // numerical path of the URL and the password is the password field of
           // the URL authority
-          "redis://:@localhost:6379/1");
+          new RedisOptions());
         client.connect()
         .onSuccess(conn -> {
           System.out.println("connected!");
